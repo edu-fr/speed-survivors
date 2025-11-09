37bc3e2a29ff8cb5605207e0be5208a5e6b130c3
Eduardo F Ribeiro <eduribeirodev@gmail.com>
2025-11-09 11:29:20 -0300

    project structure, assemblies, and scene base controllers

---
+++ speed-survivors/Assets/Scripts/Controller/Player/PlayerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Controller.Player
+{
+	public class PlayerController : MonoBehaviour
+	{
+		// Start is called once before the first execution of Update after the MonoBehaviour is created
+		void Start()
+		{
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+		}
+
+		public void SetPosition(Vector3 position)
+		{
+			transform.position = position;
+		}
+	}
+}
